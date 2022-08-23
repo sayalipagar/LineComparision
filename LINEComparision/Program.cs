f94f104fec9591e018aa598df9cb641b612696f2
sayalipagar <pagarsayali565@gmail.com>
2022-08-23 17:22:44 +0530

    [sayali]Add:EqualiLines

--- LINEComparision/Program.cs
+++ LINEComparision/Program.cs
@@ -5,18 +5,19 @@
         static void Main(string[] args)
         {
             Camparision camparision = new Camparision(1, 2, 3, 4);
-            double line1=camparision.CaculateLength();
-            Console.WriteLine("length of first line"+line1);
+            double line1 = camparision.CaculateLength();
+            Console.WriteLine("length of first line" + line1);
 
             Camparision linecamparision = new Camparision(1, 2, 3, 4);
             double line2 = camparision.CaculateLength();
             Console.WriteLine("length of second line" + line2);
-            if (line1 == line2)
-            {
-                Console.WriteLine("Line 1 and Line 2 are equal");
+            if (line1.CompareTo(line2) == 0)
+                Console.WriteLine("Both lines are same");
+            if (line1.CompareTo(line2) > 0)
+                
+                Console.WriteLine("line 1 greator than line2 ");
+                else
+                 Console.WriteLine("line 2 greator than line 1");
             }
-            else
-                Console.WriteLine("Not Equal");
-        }
     }
-}
+    }
