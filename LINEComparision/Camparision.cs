bcc40255052686ff7f5e1e026b276c7504cf2507
sayalipagar <pagarsayali565@gmail.com>
2022-08-23 16:54:55 +0530

    [Sayali]Add:LengthCalculation

--- LINEComparision/Camparision.cs
+++ LINEComparision/Camparision.cs
@@ -8,5 +8,19 @@ namespace LINEComparision
 {
     internal class Camparision
     {
+        int x1, x2, y1, y2;
+        public Camparision(int _x1, int _x2, int _y1, int _y2)
+        {
+            this.x1 = _x1;
+            this.x2 = _x2;
+            this.y1 = _y1;
+            this.y2 = _y2;
+        }
+        public void CaculateLength()
+        {
+            double linelength= Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            // double linelength1 = Math.Sqrt((x2-x1)^2 + (y2 - y1)^ 2);
+            Console.WriteLine(linelength);
+        }
     }
 }
--- LINEComparision/Program.cs
+++ LINEComparision/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Camparision camparision = new Camparision(1, 2, 3, 4);
+            camparision.CaculateLength();
+
         }
     }
 }
